918d0c1214501c02a70959fc2367abfbdb7f9f28
ylu3 <luyansasc@gmail.com>
2022-06-03 15:10:02 +0200

    update(net) tp template version

--- code/net.py
+++ code/net.py
@@ -3,17 +3,12 @@ from torchvision import models
 
 
 class ResBase(nn.Module):
-    def __init__(self, architecture, use_projection=False):
+    def __init__(self):
         super(ResBase, self).__init__()
-        if architecture == 'resnet50':
-            model_resnet = models.resnet50(pretrained=True)
-        elif architecture == 'resnet18':
-            model_resnet = models.resnet18(pretrained=True)
-        else:
-            raise ValueError("{} is not a valid known ResNet architecture.".format(architecture))
+        # Initialize pre-trained resnet18
+        model_resnet = models.resnet18(pretrained=True)
 
-        self.use_projection = use_projection
-
+        # "Steal" pretrained layers from the torchvision pretrained Resnet18
         self.conv1 = model_resnet.conv1
         self.bn1 = model_resnet.bn1
         self.relu = model_resnet.relu
@@ -34,15 +29,18 @@ class ResBase(nn.Module):
         x = self.layer3(x)
 
         x = self.layer4(x)
+        # Non-pooled tensor
         x_p = x
         x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
+        # Flatten the pooled tensor
+        x = x.flatten(start_dim=1)
 
+        # Return both
         return x, x_p
 
 
 class ResClassifier(nn.Module):
-    def __init__(self, input_dim=2048, class_num=51, extract=True, dropout_p=0.5):
+    def __init__(self, input_dim=1024, class_num=47, dropout_p=0.5):
         super(ResClassifier, self).__init__()
         self.fc1 = nn.Sequential(
             nn.Linear(input_dim, 1000),
@@ -51,15 +49,12 @@ class ResClassifier(nn.Module):
             nn.Dropout(p=dropout_p)
         )
         self.fc2 = nn.Linear(1000, class_num)
-        self.extract = extract
         self.dropout_p = dropout_p
 
     def forward(self, x):
         emb = self.fc1(x)
         logit = self.fc2(emb)
 
-        if self.extract:
-            return emb, logit
         return logit
 
 
@@ -70,27 +65,27 @@ class RelativeRotationClassifier(nn.Module):
         self.projection_dim = projection_dim
 
         self.conv_1x1 = nn.Sequential(
-            nn.Conv2d(self.input_dim, self.projection_dim, [1,1], stride=[1,1]),
+            nn.Conv2d(self.input_dim, self.projection_dim, (1, 1), stride=(1, 1)),
             nn.BatchNorm2d(self.projection_dim),
             nn.ReLU(inplace=True)
-            )
+        )
         self.conv_3x3 = nn.Sequential(
-            nn.Conv2d(self.projection_dim, self.projection_dim, [3,3], stride=[2,2]),
+            nn.Conv2d(self.projection_dim, self.projection_dim, (3, 3), stride=(2, 2)),
             nn.BatchNorm2d(self.projection_dim),
             nn.ReLU(inplace=True)
-            )
+        )
         self.fc1 = nn.Sequential(
-            nn.Linear(self.projection_dim*3*3, self.projection_dim),
+            nn.Linear(self.projection_dim * 3 * 3, self.projection_dim),
             nn.BatchNorm1d(self.projection_dim, affine=True),
             nn.ReLU(inplace=True),
             nn.Dropout(p=0.5)
-            )
+        )
         self.fc2 = nn.Linear(projection_dim, class_num)
 
     def forward(self, x):
         x = self.conv_1x1(x)
         x = self.conv_3x3(x)
-        x = x.view(x.size(0), -1)
+        x = x.flatten(start_dim=1)
         x = self.fc1(x)
         x = self.fc2(x)
         return x
