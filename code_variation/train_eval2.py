c358057b5b1ecd4ffee653c6a40e0e50a9998e00
ylu3 <luyansasc@gmail.com>
2022-06-05 21:12:08 +0200

    debug

--- code_variation/train_eval2.py
+++ code_variation/train_eval2.py
@@ -382,7 +382,7 @@ for epoch in range(first_epoch, args.epochs + 1):
 
                 # val_loss_rot_source += ce_loss(preds, rot_label).item()
                 val_loss_rot_source += rotation_loss(preds, rot_label).item()
-                print(val_loss_rot_target)
+                print(val_loss_rot_source)
                 # correct += (torch.argmax(preds, dim=1) == rot_label).sum().item()
                 # num_predictions += preds.shape[0]
 
